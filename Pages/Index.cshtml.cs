95ef97c6292ede659571f66c98e8401dc1fd4dc2
Christian Luke <13chrisl@gmail.com>
2024-09-12 13:01:26 -0600

    Create Cards for Dashboard, add  bootstrap styling, redirect homepage to login form, modified gitignore

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, RazorPagesMovieContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
 
+            return Redirect("/Users/Login");
+            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using RazorPagesMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
@@ -31,6 +32,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();

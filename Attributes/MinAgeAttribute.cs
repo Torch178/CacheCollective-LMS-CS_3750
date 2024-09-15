5edb702d5e662d8ebe6db9291a4ad3aae2296382
Carlos Castillo <carloscastillo@mail.weber.edu>
2024-09-15 15:37:03 -0600

    Added validation attribute minAge and database migrations.

---
+++ Attributes/MinAgeAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Attributes
+{
+    public class MinAgeAttribute : ValidationAttribute
+    {
+        private int minAge;
+
+        public MinAgeAttribute(int minAge)
+        {
+            this.minAge = minAge;
+            ErrorMessage = "You must be at least " + this.minAge + " years old!";
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if (value is DateTime birthDate)
+            {
+                var todayDate = DateTime.Today;
+                var age = todayDate.Year - birthDate.Year;
+
+                if (birthDate > todayDate.AddYears(-age)) age--; // Check if birthdate has passed
+                if (age < this.minAge) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Migrations/20240915094611_UserValidation2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserValidation2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsInstructor",
+                table: "User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsInstructor",
+                table: "User");
+        }
+    }
+}

ed8ba5c25a91c3f9c291ebac7e7736e933df762a
Carlos Castillo <carloscastillo@mail.weber.edu>
2024-09-15 15:18:31 -0600

    Added sign up form validation and new options.

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using RazorPagesMovie.Attributes;
 
 namespace RazorPagesMovie.Models
 {
@@ -20,14 +21,22 @@ namespace RazorPagesMovie.Models
         [Display(Name = "First Name")]
         [StringLength(20, MinimumLength = 1)]
         [RegularExpression(@"^[a-zA-Z]*")]
+        [Required]
         public string FirstName { get; set; }
 
         [Display(Name = "Last Name")]
         [StringLength(30, MinimumLength = 1)]
         [RegularExpression(@"^[a-zA-Z]*")]
+        [Required]
         public string LastName { get; set; }
 
         [DataType(DataType.Date)]
+        [MinAge(16)]
+        [Required]
         public DateTime Birthdate { get; set; }
+
+        [Display(Name = "Are you an instructor?")]
+        [Required(ErrorMessage = "Please select either Yes or No.")]
+        public bool? IsInstructor { get; set; }
     }
 }

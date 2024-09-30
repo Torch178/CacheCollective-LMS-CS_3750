468b80e9dafa88da5466d11c3e55dc990c89f5ae
Carlos Castillo <carloscastillo@mail.weber.edu>
2024-09-30 12:03:56 -0600

    Added Account Tab, Dashboard fixes and links, and a dynamic navbar (as well as the logout feature and numerous of other fixes to ensure proper functionality of the site in total).

---
+++ Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Linq;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Shared
+{
+    public class LayoutModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public LayoutModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User CurrentUser { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Console.WriteLine("Layout Loaded");
+            if (CurrentUser == null)
+            {
+                // Fetch user from claims
+                var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (userIdClaim == null) { return RedirectToPage("./Login"); }
+                if (!int.TryParse(userIdClaim, out var userId)) { return RedirectToPage("./Login"); } // invalid userId
+
+                var user = await _context.User.FirstOrDefaultAsync(m => m.Id == userId);
+                if (user == null) { return NotFound(); }
+
+                CurrentUser = user;
+            }
+
+            ViewData["IsInstructor"] = CurrentUser.IsInstructor;
+            Console.WriteLine(ViewData["IsInstructor"]);
+
+            return Page();
+        }
+    }
+}

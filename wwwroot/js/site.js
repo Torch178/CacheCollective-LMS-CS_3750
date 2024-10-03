8304354049045e91e87253b07c2464fa5b4ae0f9
Christian Luke <13chrisl@gmail.com>
2024-10-03 14:44:00 -0600

    SavePoint2_indexPageShenanigans

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -11,77 +11,34 @@ $('#todo-input').on('input',function(e){
     currentTodo.text = e.target.value;
    });
 */
- 
+
 
 
 //Creates Individual To-Do List Items with Delete Button
-function DrawTodo(todo) {
-    var newTodoHTML = `
-  <div class="col col-12 p-2 todo-item" todo-id="${todo.id}">
-  <div class="input-group border border-primary object-fit-contain m-1">
-  <div class="container w-75 ">
-      <h6 class="text-wrap">${todo.course}</h6>
-      <div class="text-wrap">${todo.assignment}</div>  
-      <div class="text-wrap font-weight-bold">${todo.due_date}</div>
-  </div>
-  <div class="input-group-append">
-    <button todo-id="${todo.id}" class="btn btn-outline-secondary bg-danger text-white float-right align-end" type="button" onclick="DeleteTodo(this);"
-      id="button-addon2 ">X</button>
-  </div>
-  </div>
-  </div>
-  `;
 
-    var dummy = document.createElement("DIV");
-    dummy.innerHTML = newTodoHTML;
-    document.getElementById("todo-container").appendChild(dummy.children[0]);
-
-    /*
-    //jQuery version
-     var newTodo = $.parseHTML(newTodoHTML);
-     $("#todo-container").append(newTodo);
-    */
-
-}
 
 //Main function, Renders all To-Do Items in List
-function RenderAllTodos() {
 
-    var container = document.getElementById("todo-container");
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-    /*
-    //jQuery version
-      $("todo-container").empty();
-    */
-
-
-    for (var i = 0; i < todos.length; i++) {
-        DrawTodo(todos[i]);
-    }
-}
-
-RenderAllTodos();
 
 //Deletes To-Do List Item from List, triggered by list item delete button "X"
 function DeleteTodo(button) {
-
-    var deleteID = parseInt(button.getAttribute("todo-id"));
     /*
     //jQuery version
-      var deleteID = parseInt($(button).attr("todo-id"));
+      var deleteID = $(button).attr("todo-id");
     */
+    var deleteID = button.getAttribute("todo-id");
+    var eDiv = document.getElementById(deleteID)
+   eDiv.parentNode.removeChild(eDiv)
 
-    for (let i = 0; i < todos.length; i++) {
-        if (todos[i].id === deleteID) {
-            todos[i].done = true;
-            archive.push(todos[i]);
-            todos.splice(i, 1);
-            RenderAllTodos();
-            break;
-        }
-    }
+    //for (let i = 0; i < todos.length; i++) {
+    //    if (todos[i].id === deleteID) {
+    //        todos[i].done = true;
+    //        archive.push(todos[i]);
+    //        todos.splice(i, 1);
+    //        RenderAllTodos();
+    //        break;
+    //    }
+    //}
 }
 
 function TodoChecked(id) {

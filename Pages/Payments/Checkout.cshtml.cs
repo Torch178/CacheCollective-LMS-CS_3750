6c7d1f54accd8aba98c142f809c34a7744c69fa5
Christian Luke <13chrisl@gmail.com>
2024-10-12 12:49:54 -0600

    Checkout working, Troubleshoot redirect error after checkout

--- Pages/Payments/Checkout.cshtml.cs
+++ Pages/Payments/Checkout.cshtml.cs
@@ -18,21 +18,7 @@ namespace RazorPagesMovie.Pages.Payments
             public string option { get; set; }
         }
 
-        public class Startup
-        {
-
-            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-            {
-                // This test secret API key is a placeholder. Don't include personal details in requests with this key.
-                // To see your test secret API key embedded in code samples, sign in to your Stripe account.
-                // You can also find your test secret API key at https://dashboard.stripe.com/test/apikeys.
-                StripeConfiguration.ApiKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
-                if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
-                app.UseRouting();
-                app.UseStaticFiles();
-                app.UseEndpoints(endpoints => endpoints.MapControllers());
-            }
-        }
+        
 
         public void OnGet()
         {
@@ -42,22 +28,25 @@ namespace RazorPagesMovie.Pages.Payments
 
         public IActionResult OnPost()
         {
-            StripeConfiguration.ApiKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
+            StripeConfiguration.ApiKey = "sk_test_51Q6simP6Fkhfsw4osozLyQK35jEf9YNVBsyRSyEN80Colog02BNiuhb4lg4wNN604wapVshVJvi7D5JINpJGiogY00fJyblERC";
+            //"http://localhost:7257";
             var domain = "http://localhost:7257";
             var options = new SessionCreateOptions
             {
                 LineItems = new List<SessionLineItemOptions>
+
             {
                 new SessionLineItemOptions
                 {
                 // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                Price = "{{PRICE_ID}}",
+                 
+                Price = "price_1Q99SrP6Fkhfsw4ollDuBi6d",
                 Quantity = 1,
                 },
             },
                 Mode = "payment",
-                SuccessUrl = domain + "/success.html",
-                CancelUrl = domain + "/cancel.html",
+                SuccessUrl = domain + "/Payments/Success",
+                CancelUrl = domain + "/Payments/Cancel",
             };
             var service = new SessionService();
             Session session = service.Create(options);
@@ -65,31 +54,7 @@ namespace RazorPagesMovie.Pages.Payments
             Response.Headers.Add("Location", session.Url);
             return new StatusCodeResult(303);
         }
-        public IActionResult Create()
-        {
-            
-            var domain = "http://localhost:7257";
-            var options = new SessionCreateOptions
-            {
-                LineItems = new List<SessionLineItemOptions>
-            {
-                new SessionLineItemOptions
-                {
-                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                Price = "{{PRICE_ID}}",
-                Quantity = 1,
-                },
-            },
-                Mode = "payment",
-                SuccessUrl = domain + "/success.html",
-                CancelUrl = domain + "/cancel.html",
-            };
-            var service = new SessionService();
-            Session session = service.Create(options);
-
-            Response.Headers.Add("Location", session.Url);
-            return new StatusCodeResult(303);
-        }
+        
         
     }
 }

8a1d1ca93473f23a5d282d841e88ab0ea6a77abd
Christian Luke <13chrisl@gmail.com>
2024-10-14 12:23:34 -0600

    Fixed redirect issue with StripeCheckout, loacl webhooks created with CLI

--- Pages/Payments/Success.cshtml.cs
+++ Pages/Payments/Success.cshtml.cs
@@ -10,6 +10,45 @@ namespace RazorPagesMovie.Pages.Payments
         public void OnGet()
         {
         }
-        
+
+        const string endpointSecret = "whsec_2668db2175ef44784c4016a70012468bf73deab17c5e89972263cff051426d73";
+        public async Task<IActionResult> Post()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            try
+            {
+                //var stripeEvent = EventUtility.ParseEvent(json);
+                var stripeEvent = EventUtility.ConstructEvent(json,
+                    Request.Headers["Stripe-Signature"], endpointSecret);
+
+
+                // Handle the event
+                if (stripeEvent.Type == EventTypes.PaymentIntentSucceeded)
+                {
+                    var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+                    // Then define and call a method to handle the successful payment intent.
+                    // handlePaymentIntentSucceeded(paymentIntent);
+                    return Page();
+                }
+                else if (stripeEvent.Type == EventTypes.PaymentMethodAttached)
+                {
+                    var paymentMethod = stripeEvent.Data.Object as PaymentMethod;
+                    // Then define and call a method to handle the successful attachment of a PaymentMethod.
+                    // handlePaymentMethodAttached(paymentMethod);
+                }
+                // ... handle other event types
+                else
+                {
+                    // Unexpected event type
+                    Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+                }
+                return Page();
+            }
+            catch (StripeException e)
+            {
+                return BadRequest();
+            }
+        }
     }
 }

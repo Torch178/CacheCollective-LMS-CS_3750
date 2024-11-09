528b9a15c02c339629710e0c2210bcfa2deba5e6
Christian Luke <13chrisl@gmail.com>
2024-11-09 13:00:45 -0700

    Fixing elem selection issue in Selenium Tests

--- src/CacheCollectiveTest/SeleniumTests/UserUpdatesProfileTest.cs
+++ src/CacheCollectiveTest/SeleniumTests/UserUpdatesProfileTest.cs
@@ -39,7 +39,7 @@ namespace CacheCollectiveTest.SeleniumTests
             //navigate to profile
             driver.Navigate().GoToUrl("https://cachecollective.azurewebsites.net/Users/Profile");
             Thread.Sleep(1000);
-            var editLink = driver.FindElement(By.Id("edit_profile_link"));
+            var editLink = driver.FindElement(By.LinkText("Edit"));
 
             //click on edit profile link
             editLink.Click();
@@ -47,11 +47,11 @@ namespace CacheCollectiveTest.SeleniumTests
             Assert.IsTrue(driver.Title.Contains("Edit Profile"));
 
             //enter new information into edit profile form
-            submitButton = driver.FindElement(By.ClassName("btn"));
-            var passInput1 = driver.FindElement(By.Id("fname"));
-            var passInput2 = driver.FindElement(By.Id("lname"));
-            var passInput3 = driver.FindElement(By.Id("apartmentNum"));
-            var passInput4 = driver.FindElement(By.Id("city"));
+            submitButton = driver.FindElement(By.Id("btn"));
+            var passInput1 = driver.FindElement(By.Id("ViewModel_FirstName"));
+            var passInput2 = driver.FindElement(By.Id("ViewModel_LastName"));
+            var passInput3 = driver.FindElement(By.Id("ViewModel_ApartmentNum"));
+            var passInput4 = driver.FindElement(By.Id("ViewModel_City"));
             passInput1.Clear();
             passInput2.Clear();
             passInput3.Clear();
@@ -68,11 +68,12 @@ namespace CacheCollectiveTest.SeleniumTests
             Thread.Sleep(1000);
 
             //Assert that the profile page reloads with new information in relevant fields
-            Assert.IsTrue(driver.Title.Equals("Profile"));
-            var fname = driver.FindElement(By.Id("fname"));
-            var lname = driver.FindElement(By.Id("lname"));
-            var apartmentNum = driver.FindElement(By.Id("apartmentNum"));
-            var city = driver.FindElement(By.Id("city"));
+            Assert.IsTrue(driver.Title.Contains("Profile"));
+            Thread.Sleep(1000);
+            var fname = driver.FindElement(By.CssSelector("#fname"));
+            var lname = driver.FindElement(By.CssSelector("#lname"));
+            var apartmentNum = driver.FindElement(By.CssSelector("#apartmentNum"));
+            var city = driver.FindElement(By.CssSelector("#city"));
             Assert.IsTrue(fname.Text.Contains("Michael"));
             Assert.IsTrue(lname.Text.Contains("Reeves"));
             Assert.IsTrue(apartmentNum.Text.Contains("2B"));
